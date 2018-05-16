f9f7471d480476f267c0c7283695cf5f7b5aa739
Max Falkenstein <mrfalken@iu.edu>
2018-05-16 14:27:13 -0400

    makes list for it

--- app.js
+++ app.js
@@ -1,55 +1,19 @@
-const form = document.querySelector('form#userForm')
+//const form = document.querySelector('form#userForm')
+const addMovie = function(ev) {
+    ev.preventDefault()
+    const movieName = form.movieName.value
+    const users = document.querySelector('#users')
 
-function renderColor(color) {
-  const colorDiv = document.createElement('div')
-  colorDiv.style.backgroundColor = color
-  colorDiv.style.width = '6rem'
-  colorDiv.style.height = '3rem'
+    const list = document.createElement('ul')
+    const listOfMovies = document.createElement('li')
+    listOfMovies.textContent = `${movieName}`
+  
+    users.appendChild(list)
 
-  return colorDiv
+    list.appendChild(listOfMovies)
+    form.reset()
+    form.movieName.focus()
 }
-
-function renderListItem(label, value) {
-  const item = document.createElement('li') //dt contains the label, dd contains the description
-
-  const term = document.createElement('dt')
-  term.textContent = label
-
-  const description = document.createElement('dd')
-  try {
-    description.appendChild(value)
-  } catch(e) {
-    description.textContent += value
-  }
-  item.appendChild(term)
-  item.appendChild(description)
-  return item
-}
-
-function renderList(data) {
-  const list = document.createElement('dl')
-  Object.keys(data).forEach(label => {
-    const item = renderListItem(label, data[label])
-    list.appendChild(item)
-  })
-  return list
-}
-
-const handleSubmit = function(ev) {
-  ev.preventDefault()
-  const form = ev.target
-  const user = {
-    'Name': form.userName.value,
-    'Age': form.age.value,
-    'Favorite Color': renderColor(form.favoriteColor.value),
-  }
-
-  const list = renderList(user)
-  const users = document.querySelector('#users')
-  users.appendChild(list)
-
-  form.reset()
-  form.userName.focus()
-}
-
-form.addEventListener('submit', handleSubmit)
+const array = []
+const form = document.querySelector('#userForm')
+form.addEventListener('submit', addMovie)
