5f07ffde5e8e82228c4dc939e0c7a2aa608ce8c1
Max Falkenstein <mrfalken@iu.edu>
2018-05-17 09:01:07 -0400

    show submission morning of the 17th

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ const addMovie = function(ev) {
     form.reset()
     form.movieName.focus()
 }
+
 const arrayOfMovies = []
+const removeItem = function() { 
+    list.removeChild(listOfMovies)
+    for(var i = arrayOfMovies.length - 2; i < arrayOfMovies.length-1; i++) {
+           arrayOfMovies.splice(i, 1);
+        }
+    }
 const form = document.querySelector('#userForm')
 form.addEventListener('submit', addMovie)
+form.addEventListener('remove', removeItem)
