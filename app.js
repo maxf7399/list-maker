2b53f1f5757dae91251ee2b700e676d21bb628d9
Max Falkenstein <mrfalken@iu.edu>
2018-05-17 09:18:32 -0400

    removed the delete button since I was not able to get it to work for now

--- app.js
+++ app.js
@@ -17,12 +17,14 @@ const addMovie = function(ev) {
 }
 
 const arrayOfMovies = []
+/*
 const removeItem = function() { 
     list.removeChild(listOfMovies)
     for(var i = arrayOfMovies.length - 2; i < arrayOfMovies.length-1; i++) {
            arrayOfMovies.splice(i, 1);
         }
     }
+    */
 const form = document.querySelector('#userForm')
 form.addEventListener('submit', addMovie)
-form.addEventListener('remove', removeItem)
+//form.addEventListener('remove', removeItem)
