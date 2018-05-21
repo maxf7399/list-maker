d17568dcc677d1d6f26c552b755a68c00cb1bc59
Max Falkenstein <mrfalken@iu.edu>
2018-05-21 10:03:01 -0400

    favorite the list item if clicked on

--- app.js
+++ app.js
@@ -1,74 +1,69 @@
-const app={
-    init(selectors) {
-        this.max=0
-        this.list=document.querySelector(selectors.listSelector)
-        this.flicks=[]
-        this.template=document.querySelector(selectors.templateSelector)
-        document   
-            .querySelector(selectors.formSelector)
-            .addEventListener('submit', (ev) => {
-                ev.preventDefault()
-                this.handleSubmit(ev)
-            })
-    },
-    removeFlick(flick, ev) {
-        const item = ev.target.closest('.flick')
-        item.remove()
-
-        const i = this.flicks.indexOf(flick)
-        this.flicks.splice(i, 1)
-    },
-
-    favoriteListItem(ev){
-        const button = ev.target
-        const flick = button.parentNode.parentNode
-        flick.style.backgroundColor='Yellow'
-        for(let i=0; i<this.flicks.length; i++)
-        {
-            if(this.flicks[i].id===flick.dataset.id)
-            {
-                this.flicks[i].fav=true
-            }
-        }
-    },
-
-    renderListItem(flick) {
-        const item = this.template.cloneNode(true)
-        item.classList.remove('template')
-        item.dataset.id = flick.id
-        item
-          .querySelector('.flickName')
-          .textContent = flick.name
-        item
-          .querySelector('.remove.button')
-          .addEventListener('click', this.removeFlick.bind(this, flick))
-        item
-          .querySelector('.warning.button')
-          .addEventListener('click', (ev)=>{
-            ev.preventDefault()
-            this.favoriteListItem(ev)
+class App {
+    constructor(selectors) {
+      this.flicks = []
+      this.max = 0
+      this.list = document.querySelector(selectors.listSelector)
+      this.template = document.querySelector(selectors.templateSelector)
+  
+      document
+        .querySelector(selectors.formSelector)
+        .addEventListener('submit', ev => {
+          ev.preventDefault()
+          this.handleSubmit(ev)
         })
-        return item
-    },
-
+    }
+  
+    removeFlick(flick, ev) {
+      // remove from the DOM
+      const item = ev.target.closest('.flick')
+      item.remove()
+  
+      // remove from the array
+      const i = this.flicks.indexOf(flick)
+      this.flicks.splice(i, 1)
+    }
+    favFlick(flick, ev) {
+        const item = ev.target.closest('.flick')
+        flick.fav = item.classList.toggle('fav')
+    }
+  
+    renderListItem(flick) {
+      const item = this.template.cloneNode(true)
+      item.classList.remove('template')
+      item.dataset.id = flick.id
+      item
+        .querySelector('.flickName')
+        .textContent = flick.name
+  
+      item
+        .querySelector('.remove.button')
+        .addEventListener('click', this.removeFlick.bind(this, flick))
+      item
+        .querySelector('.fav.button')
+        .addEventListener('click', this.favFlick.bind(this,flick))
+  
+      return item
+    }
+  
     handleSubmit(ev) {
-        const f=ev.target
-        const flick = {
-            id: ++this.max,
-            name: f.flickName.value,
-            fav: false, 
-        }
-        
-        this.flicks.unshift(flick)
-        console.log(this.flicks)
-        const item=this.renderListItem(flick) 
-        this.list.insertBefore(item, this.list.firstElementChild)
-        console.log(flick)
-        f.reset()
-    },
-}
-app.init({
+      const f = ev.target
+      const flick = {
+        id: ++this.max,
+        name: f.flickName.value,
+        fav: false,
+      }
+  
+      this.flicks.unshift(flick)
+  
+      const item = this.renderListItem(flick)
+      this.list.insertBefore(item, this.list.firstElementChild)
+  
+      f.reset()
+    }
+  }
+  
+  const app = new App({
     formSelector: '#flickForm',
     listSelector: '#flickList',
     templateSelector: '.flick.template',
-})
+  })
