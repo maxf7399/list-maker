14192d96b69e985c49a983268a6020f6e4f664b6
Max Falkenstein <mrfalken@iu.edu>
2018-05-21 10:45:32 -0400

    enter button works with it now

--- app.js
+++ app.js
@@ -22,25 +22,72 @@ class App {
       const i = this.flicks.indexOf(flick)
       this.flicks.splice(i, 1)
     }
+  
     favFlick(flick, ev) {
-        const item = ev.target.closest('.flick')
-        flick.fav = item.classList.toggle('fav')
+      const item = ev.target.closest('.flick')
+      flick.fav = item.classList.toggle('fav')
+    }
+  
+    toggleEditable(flick, ev) {
+      const item = ev.target.closest('.flick')
+      const btn = item.querySelector('.edit.button')
+      const nameField = item.querySelector('.flickName')
+  
+      if (nameField.isContentEditable) {
+        // make it no longer editable
+        nameField.contentEditable = false
+        btn.textContent = 'edit'
+        btn.classList.remove('success')
+  
+        // save changes
+        flick.name = nameField.textContent
+      } else {
+        // make it editable
+        nameField.contentEditable = true
+        nameField.focus()
+        btn.textContent = 'save'
+        btn.classList.add('success')
+      }
+    }
+  
+    saveOnEnter(flick, ev) {
+      if (ev.key === 'Enter') {
+        this.toggleEditable(flick, ev)
+      }
     }
   
     renderListItem(flick) {
       const item = this.template.cloneNode(true)
       item.classList.remove('template')
       item.dataset.id = flick.id
-      item
-        .querySelector('.flickName')
-        .textContent = flick.name
+  
+      const nameSpan = item.querySelector('.flickName')
+      nameSpan.textContent = flick.name
+      nameSpan.addEventListener(
+        'keypress',
+        this.saveOnEnter.bind(this, flick)
+      )
   
       item
         .querySelector('.remove.button')
-        .addEventListener('click', this.removeFlick.bind(this, flick))
+        .addEventListener(
+          'click',
+          this.removeFlick.bind(this, flick)
+        )
+  
       item
         .querySelector('.fav.button')
-        .addEventListener('click', this.favFlick.bind(this,flick))
+        .addEventListener(
+          'click',
+          this.favFlick.bind(this, flick)
+        )
+  
+      item
+        .querySelector('.edit.button')
+        .addEventListener(
+          'click',
+          this.toggleEditable.bind(this, flick)
+        )
   
       return item
     }
